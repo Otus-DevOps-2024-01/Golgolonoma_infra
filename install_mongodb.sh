a069894f77a6ca04b1332c60c85130dc4f48541a
Golgolonoma <friorianf@gmail.com>
2024-02-28 21:14:27 +0300

    HW-4 fix install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# Update the package listing
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates gnupg -y
-sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc
-sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse"
-sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+# Install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install -y apt-transport-https ca-certificates gnupg
+
+# Import the MongoDB public GPG Key:
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+
+# Add the MongoDB repository
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
