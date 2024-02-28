898d6fbaa76699d8c7c318ae5115a3c66144b190
Golgolonoma <friorianf@gmail.com>
2024-02-28 20:02:19 +0300

    HW-4 fix install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt install -y mongodb-org build-essential
