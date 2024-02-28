449723a618e4748eb6e9382a4d7218f3d82c9c8f
Golgolonoma <friorianf@gmail.com>
2024-02-28 20:38:55 +0300

    HW-4 fix install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+# Update the package listing
+sudo apt-get update
+# Install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install -y apt-transport-https ca-certificates gnupg
 
-sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - && \
-sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list && \
-sudo apt-get update && sudo apt-get install -y mongodb-org
-sudo systemctl start mongod &&  sudo systemctl enable mongod
+# Import the MongoDB public GPG Key:
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+
+# Add the MongoDB repository
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+# Update the package listing again, now with the new repository added
+sudo apt-get update
+
+# Install MongoDB packages
+sudo apt-get install -y mongodb-org
+
+# Start MongoDB and enable it to start on boot
+sudo systemctl start mongod
+sudo systemctl enable mongod
