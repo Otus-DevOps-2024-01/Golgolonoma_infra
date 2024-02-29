b67591c2f61f5cc6ce0399278a8dfad1dcd809dc
Golgolonoma <friorianf@gmail.com>
2024-02-29 11:00:12 +0300

    HW-4 fix install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -12,5 +12,5 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongod
 
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+sudo systemctl service start mongod
+sudo systemctl service enable mongod
