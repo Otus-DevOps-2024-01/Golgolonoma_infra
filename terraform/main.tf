e7ea32657b269470c68515d3fa5aa9871a7cb595
Golgolonoma <friorianf@gmail.com>
2024-03-18 09:39:19 +0300

    HW-6 terraform-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 0.13"
-
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.35"
-    }
-  }
-}
-
-
-
-
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
