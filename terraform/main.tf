a21bbd9939ec9c0e633994a8546a4fe4be005fc1
Golgolonoma <friorianf@gmail.com>
2024-05-07 14:23:42 +0300

    HW-6 terraform-2 fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
-      version = "0.112.0"  // Specify the appropriate version you need.
+      source  = "yandex-cloud/yandex"
+      version = "0.112.0"
     }
   }
 }
--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.112.0"
+    }
+  }
+}
+
 resource "yandex_vpc_network" "app_network" {
   name = "app-network"
 }
