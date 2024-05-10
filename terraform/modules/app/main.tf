b40a63a2d719c713db98a909fc5e45bf0586b7fd
Golgolonoma <friorianf@gmail.com>
2024-05-10 17:16:48 +0300

    HW-7 ansible-1 fix

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.112.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     yandex = {
+#       source  = "yandex-cloud/yandex"
+#       version = "0.112.0"
+#     }
+#   }
+# }
 
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
